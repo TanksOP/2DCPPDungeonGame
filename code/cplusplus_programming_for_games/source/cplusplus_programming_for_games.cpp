fb37ce0c191ba861a5f2472e208311cb6164facd
James Wilson <james.wilson6@mail.bcu.ac.uk>
2021-09-21 18:34:25 +0100

    Moving the SDL2 folder

--- code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
+++ code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
@@ -1,6 +1,8 @@
 // cplusplus_programming_for_games.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
 int main()
 {
