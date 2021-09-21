9f5d6529f8f057e83846e3005af4dba1b84ef9df
James Wilson <james.wilson6@mail.bcu.ac.uk>
2021-09-21 18:59:44 +0100

    test code for sdl2

--- code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
+++ code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
@@ -4,9 +4,27 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
-int main()
+//int main()
+//{
+//    std::cout << "Game Over!" << std::endl;
+//    std::cout << "Or is it! ......." << std::endl;
+//    return 0;
+//}
+
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Game Over!" << std::endl;
-    std::cout << "Or is it! ......." << std::endl;
+
+    // retutns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1000, 1000, 0);
+    while (1)
+        ;
+
     return 0;
 }
