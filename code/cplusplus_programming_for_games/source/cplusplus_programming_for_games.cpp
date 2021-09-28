eb9f412f4f1c24bdae3fdcba68b24bb5425172e1
James Wilson <james.wilson6@mail.bcu.ac.uk>
2021-09-28 17:25:07 +0100

    trying a new set of code in sld2

--- code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
+++ code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
@@ -12,19 +12,52 @@
 //}
 
 
-int main(int argc, char* argv[])
+//int main(int argc, char* argv[])
+//{
+//
+//    // retutns zero on success else non-zero
+//    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+//        printf("error initializing SDL: %s\n", SDL_GetError());
+//    }
+//    SDL_Window* win = SDL_CreateWindow("GAME",
+//        SDL_WINDOWPOS_CENTERED,
+//        SDL_WINDOWPOS_CENTERED,
+//        1000, 1000, 0);
+//    while (1)
+//        ;
+//
+//    return 0;
+//}
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+int main()
 {
+	SDL_Window* window = nullptr;
+	SDL_Surface* screenSurface = nullptr;
 
-    // retutns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    SDL_Window* win = SDL_CreateWindow("GAME",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        1000, 1000, 0);
-    while (1)
-        ;
-
-    return 0;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "could not initialize SDL2!" << std::endl;
+		std::cout << SDL_GetError() << std::endl;
+		return 1;
+	}
+	window = SDL_CreateWindow(
+		"hello_sdl2",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		SDL_WINDOW_SHOWN
+	);
+	if (window == NULL) {
+		std::cout << "could not initialise window!" << std::endl;
+		std::cout << SDL_GetError() << std::endl;
+		return 1;
+	}
+	screenSurface = SDL_GetWindowSurface(window);
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 144, 238, 144));
+	SDL_UpdateWindowSurface(window);
+	SDL_Delay(5000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 }
