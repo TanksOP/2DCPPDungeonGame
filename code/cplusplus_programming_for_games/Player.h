a9938cbd2923add0d6dd468ebd255a6070503864
James Wilson <james.wilson6@mail.bcu.ac.uk>
2021-10-05 19:04:42 +0100

    changes

--- code/cplusplus_programming_for_games/Player.h
+++ code/cplusplus_programming_for_games/Player.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
 class Player
 {
+public:
+	Player();
+	void init();
+	void render();
+	void update();
+	void clean();
+
+private:
+	SDL_Renderer* renderer = nullptr;
+	SDL_Texture* texture = nullptr;
 };
 
